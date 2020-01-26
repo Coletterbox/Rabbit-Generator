5d8e817d8e85a2388fc3918e6f179d3b7e93ba16
Colette Shaw <CShaw@spartaglobal.com>
2020-01-26 05:26:40 +0000

    Add zombie function

--- Rabbits1/Rabbits1/MainWindow.xaml.cs
+++ Rabbits1/Rabbits1/MainWindow.xaml.cs
@@ -42,9 +42,13 @@ namespace Rabbits1
 
         private void Button100Rabbits_Click(object sender, RoutedEventArgs e)
         {
+            //int currentRabbitCount = rabbits.Count;
+            //Console.WriteLine(currentRabbitCount);
+            //ListBox100Rabbits.Items.Add(currentRabbitCount);
             for (int i = 0; i < 100; i++)
             {
                 var rabbit = new Rabbit();
+                //rabbit.RabbitName = "Rabbit" + " " + (currentRabbitCount + i + 1) + " (" + GenerateName() + ")" + currentRabbitCount;
                 rabbit.RabbitName = "Rabbit" + " " + (i + 1) + " (" + GenerateName() + ")";
                 rabbit.isLiving = true;
                 rabbit.isZombie = false;
@@ -67,7 +71,7 @@ namespace Rabbits1
                 {
                     rabbit.Age++;
                 }
-                ListBoxAgeRabbits100Times.Items.Add(rabbit.RabbitName + ", " + rabbit.Age + " years old");
+                ListBoxAgeRabbits100Times.Items.Add(rabbit.RabbitName + " is now " + rabbit.Age + " years old!");
             }
 
             // every century, there is a wave of zombies
@@ -84,15 +88,22 @@ namespace Rabbits1
 
         int deadRabbits = 0;
         int liveRabbits = 0;
+        int zombieRabbits = 0;
         public void CountRabbits_Click(object sender, RoutedEventArgs e)
         {
             deadRabbits = 0;
             liveRabbits = 0;
+            zombieRabbits = 0;
             foreach (var rabbit in rabbits)
             {
                 //for (int i = 0; i < rabbits.Count; i++)
                 //{
-                    if (rabbit.Age > 10)
+                    if (rabbit.isZombie == true)
+                    {
+                        zombieRabbits++;
+                        deadRabbits--;
+                    }
+                    if (rabbit.Age > 10 && rabbit.isZombie == false)
                     {
                         rabbit.isLiving = false;
                         deadRabbits++;
@@ -103,9 +114,11 @@ namespace Rabbits1
                     }
                 //}
             }
+            ListBoxCountRabbits.Items.Add("Update:");
             ListBoxCountRabbits.Items.Add("There are " + liveRabbits + " living rabbits.");
             ListBoxCountRabbits.Items.Add("There are " + deadRabbits + " dead rabbits.");
-            //ListBoxCountRabbits.Items.Add("There are " + zombieRabbits + " zombie rabbits.");
+            ListBoxCountRabbits.Items.Add("There are " + zombieRabbits + " zombie rabbits.");
+            ListBoxCountRabbits.Items.Add("");
         }
 
         private void CountRabbits_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -119,16 +132,42 @@ namespace Rabbits1
                 rabbit.Age++;
                 if (rabbit.Age == 1)
                 {
-                    ListBoxAgeRabbitsOnce.Items.Add(rabbit.RabbitName + ", " + rabbit.Age + " year old");
+                    ListBoxAgeRabbitsOnce.Items.Add(rabbit.RabbitName + " is now " + rabbit.Age + " year old!");
                 }
                 else
                 {
-                    ListBoxAgeRabbitsOnce.Items.Add(rabbit.RabbitName + ", " + rabbit.Age + " years old");
+                    ListBoxAgeRabbitsOnce.Items.Add(rabbit.RabbitName + " is now " + rabbit.Age + " years old!");
                 }
             }
         }
 
         private void ListBoxAgeRabbitsOnce_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ButtonZombieProblem_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var rabbit in rabbits)
+            {
+                // recheck if rabbit should still be living - only necessary if it has aged after the last count - this could be more cleanly implemented
+                if (rabbit.Age > 10)
+                {
+                    rabbit.isLiving = false;
+                }
+                if (rabbit.isLiving == false)
+                {
+                    Random rand = new Random();
+                    int zombieChance = rand.Next(0, 10);
+                    if (zombieChance % 7 == 0)
+                    {
+                        rabbit.isZombie = true;
+                    }
+                }
+            }
+        }
+
+        private void ListBoxZombieProblem_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
         }
